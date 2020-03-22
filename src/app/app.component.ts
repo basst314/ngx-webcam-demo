02159a358d133b647d06b5a189a9572a5689f80e
Sebastian Schlote <sebastian.schlote@gmail.com>
2020-03-21 19:47:17 -0700

    update to Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-import {Observable} from 'rxjs/Observable';
+import {Subject, Observable} from 'rxjs';
 import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
